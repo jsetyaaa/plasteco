b514cff011fa958987c0e7cf72c7ad1b9b9eef73
jsetyaaa <jsetyaaa@gmail.com>
2024-05-09 14:10:20 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'font-color': '#254A45',
+      'bg-color': '#F3FAF7',
+      'card-color': '#D8EFE8',
+      'button-color': '#3E8E7E'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
